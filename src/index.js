2b5bf27807109e9e45f441dd35021a61db952d3c
aditigupta <aditi.gupta@deeporion.com>
2023-05-11 17:17:00 +0530

    one comment is added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+//import provider to access the store in root folder.
 import { Provider } from "react-redux";
 import { store } from "./state/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
