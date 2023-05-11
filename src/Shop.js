b57af5017cafe6355db4e52eefff3d45553952b6
aditigupta <aditi.gupta@deeporion.com>
2023-05-11 15:16:48 +0530

    destructuring of array

--- src/Shop.js
+++ src/Shop.js
@@ -4,20 +4,23 @@ import { actionCreators } from "./state/index";
 import { bindActionCreators } from "redux";
 const Shop = () => {
   const dispatch = useDispatch();
-  const actions = bindActionCreators(actionCreators, dispatch);
+  const { withdrawMoney, depositMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
   return (
     <div>
       <h2>Deposit/Withdraw Money</h2>
       <button
         className="btn btn-primary mx-2"
-        onClick={() => actions.withdrawMoney(100)}
+        onClick={() => withdrawMoney(100)}
       >
         -
       </button>
       Update Balance
       <button
         className="btn btn-primary mx-2"
-        onClick={() => actions.depositMoney(100)}
+        onClick={() => depositMoney(100)}
       >
         +
       </button>
